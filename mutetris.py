07608f3da0815ee64ffd6efb4b282c815b0abb51
FranciscoDA <franciscoda@outlook.com>
2018-09-19 08:57:58 -0300

    make pieces spawn above the screen

--- mutetris.py
+++ mutetris.py
@@ -22,25 +22,38 @@ ANSI_clear_above = '\033[0J'
 ANSI_clear_below = '\033[1J'
 ANSI_cursor_position = lambda row,col: '\033[' + str(row) + ';' + str(col) + 'H'
 
-board_w = 10
-board_h = 20
+class Board:
+	def __init__(self, w, h):
+		self.w, self.h = w, h
+		self.data = [None for _ in range(w*h)]
+	def in_bounds(self, x, y): return 0 <= x < self.w and 0 <= y < self.h
+	def get(self, x, y):
+		return self.data[y*self.w+x] if self.in_bounds(x,y) else None
+	def set(self, x, y, v):
+		if self.in_bounds(x, y):
+			self.data[y*self.w+x] = v
+		else:
+			raise IndexError()
+	def clear(self, y):
+		self.data[self.w:(y+1)*self.w] = self.data[0:y*self.w]
+		self.data[0:self.w] = [None] * self.w
 
 pieces = [ # all piece types are defined as a square map of objects
 	[
-		None,    None,    Col.red, None,
-		None,    None,    Col.red, None,
-		None,    None,    Col.red, None,
-		None,    None,    Col.red, None,
+		Col.red,
+		Col.red,
+		Col.red,
+		Col.red,
 	],
 	[
-		Col.ylw, Col.ylw, None,
-		Col.ylw, None,    None,
-		Col.ylw, None,    None,
+		Col.ylw, Col.ylw,
+		Col.ylw, None,
+		Col.ylw, None,
 	],
 	[
-		None,    Col.pur, Col.pur,
-		None,    None,    Col.pur,
-		None,    None,    Col.pur,
+		Col.pur, Col.pur,
+		None,    Col.pur,
+		None,    Col.pur,
 	],
 	[
 		Col.blu, Col.blu,
@@ -49,36 +62,33 @@ pieces = [ # all piece types are defined as a square map of objects
 	[
 		None,    Col.grn, None,
 		Col.grn, Col.grn, Col.grn,
-		None,    None,    None,
 	],
 	[
 		None,    Col.cyn, Col.cyn,
 		Col.cyn, Col.cyn, None,
-		None,    None,    None,
 	],
 	[
 		Col.org, Col.org, None,
 		None,    Col.org, Col.org,
-		None,    None,    None,
 	],
 ]
-piece_size = lambda piece_type: int(len(piece_type)**0.5)
+piece_size = [(4, 1), (3, 2), (3, 2), (2,2), (2,3), (2,3), (2, 3)]
 
 def draw(board, piece_type, piece_x, piece_y, piece_r, piece_next, player_score):
 	buf = ANSI_cursor_position(1,1)
 	block_width = 2
-	active_block = '\033[38;5;7m(\033[38;5;0m)'
+	active_block = '\033[38;5;7m[\033[38;5;0m]'
 	inactive_block = active_block
 	blank = '  '
-	shadow = '()'
+	shadow = '[]'
 	shadow_x, shadow_y = piece_x, piece_y
 	while canmove(board, piece_type, shadow_x, shadow_y+1, piece_r):
 		shadow_y += 1
 
 	# draw the main area
-	for y in range(board_h):
-		for x in range(board_w):
-			ch = board[y*board_w+x]
+	for y in range(board.h):
+		for x in range(board.w):
+			ch = board.get(x, y)
 			if ch:
 				buf += ch.value + inactive_block + ANSI_reset
 			else:
@@ -94,9 +104,9 @@ def draw(board, piece_type, piece_x, piece_y, piece_r, piece_next, player_score)
 		buf += '|\n'
 
 	# draw the next piece
-	buf += ANSI_cursor_position(2, (board_w+1)*block_width+1) + 'Next:'
+	buf += ANSI_cursor_position(2, (board.w+1)*block_width+1) + 'Next:'
 	for py in range(5):
-		buf += ANSI_cursor_position(3+py, (board_w+1) * block_width + 1)
+		buf += ANSI_cursor_position(3+py, (board.w+1) * block_width + 1)
 		for px in range(5):
 			ch = getpieceblock(piece_next, px, py, 0)
 			if ch is not None:
@@ -105,9 +115,9 @@ def draw(board, piece_type, piece_x, piece_y, piece_r, piece_next, player_score)
 				buf += blank
 
 	# draw the score
-	buf += ANSI_cursor_position(12, (board_w+1)*block_width+1)
+	buf += ANSI_cursor_position(12, (board.w+1)*block_width+1)
 	buf += 'Score: ' + str(player_score)
-	buf += ANSI_cursor_position(board_h+1, 1)
+	buf += ANSI_cursor_position(board.h+1, 1)
 
 	# write buffer to stdout
 	print(buf, end='', flush=True)
@@ -128,26 +138,29 @@ def getinput(timeout):
 
 def canmove(board, piece_type, piece_x, piece_y, piece_r):
 	return all(
-		pblk is None or 0 <= px+piece_x < board_w and py+piece_y < board_h and board[(py+piece_y)*board_w+px+piece_x] is None
+		pblk is None or 0 <= px+piece_x < board.w and py+piece_y < board.h and board.get(px+piece_x, py+piece_y) is None
 		for px, py, pblk in iterpieceblocks(piece_type, piece_r)
 	)
 
 def getpieceblock(piece_type, px, py, piece_r):
 	# get a block inside a (possibly rotated) piece from
 	# the px,py coordinates, which are relative to the top-left of the piece
-	sz = piece_size(piece_type)
-	if px < 0 or px >= sz or py < 0 or py >= sz:
+	piece_h, piece_w = piece_size[piece_type]
+	if piece_r%2 == 1: px, py = py, px
+	if 1 <= piece_r <= 2: py = piece_h-1-py
+	if 2 <= piece_r <= 3: px = piece_w-1-px
+
+	if px < 0 or px >= piece_w or py < 0 or py >= piece_h:
 		return None
-	if piece_r == 0:   return piece_type[py*sz+px]
-	elif piece_r == 1: return piece_type[(sz-px-1)*sz+py]
-	elif piece_r == 2: return piece_type[(sz-py-1)*sz+(sz-px-1)]
-	else:              return piece_type[px*sz+(sz-py-1)]
+	else:
+		return pieces[piece_type][py*piece_w+px]
 
 def iterpieceblocks(piece_type, piece_r):
 	# iterate all the sub blocks of a piece
+	piece_h, piece_w = piece_size[piece_type]
+	if piece_r%2 == 1: piece_h, piece_w = piece_w, piece_h
 	return (
-		(px, py, getpieceblock(piece_type, px, py, piece_r))
-		for py in range(piece_size(piece_type)) for px in range(piece_size(piece_type))
+		(px, py, getpieceblock(piece_type, px, py, piece_r)) for py in range(piece_h) for px in range(piece_w)
 	)
 
 # decrease the input timer multiplicatively for every 100 points
@@ -162,14 +175,15 @@ KEY_SPACEBAR = b' '
 
 if __name__ == '__main__':
 	random.seed(time.time())
-	board = [None for i in range(board_w*board_h)]
+	board = Board(10, 20)
 	exit = False
 
 	player_score = 0
 	piece_counter = 1
-	piece_type = random.choice(pieces)
-	piece_x, piece_y, piece_r = board_w//2 - piece_size(piece_type)//2, 0, 0
-	piece_next = random.choice(pieces)
+	piece_type = random.randrange(len(pieces))
+	piece_h, piece_w = piece_size[piece_type]
+	piece_x, piece_y, piece_r = board.w//2 - piece_w//2, 1-piece_h, 0
+	piece_next = random.randrange(len(pieces))
 
 	input_bucket = input_bucket_max(player_score)
 	print(ANSI_clear_all, flush=True)
@@ -196,29 +210,30 @@ if __name__ == '__main__':
 			if inp == KEY_DOWN or inp == None:
 				max_drop = 1
 			elif inp == KEY_SPACEBAR:
-				max_drop = board_h
+				max_drop = board.h + piece_h
 			for _ in range(max_drop):
 				if canmove(board, piece_type, piece_x, piece_y + 1, piece_r):
 					piece_y += 1
 				else:
-					if piece_y <= 0:
-						exit = True
 					for px, py, pblk in iterpieceblocks(piece_type, piece_r):
 						if pblk is not None:
-							board[(py+piece_y)*board_w+px+piece_x] = pblk
+							if py+piece_y < 0:
+								exit = True # game over
+								break
+							board.set(px+piece_x, py+piece_y, pblk)
 					piece_type = piece_next
-					piece_x, piece_y, piece_r = board_w//2 - piece_size(piece_type)//2, 0, 0
-					piece_next = random.choice(pieces)
+					piece_h, piece_w = piece_size[piece_type]
+					piece_x, piece_y, piece_r = board.w//2 - piece_w//2, 1-piece_h, 0
+					piece_next = random.randrange(len(pieces))
 					piece_counter += 1
 					if piece_counter % 5 == 0:
 						random.seed(time.time())
 					multiplier = 1
 					# check for a complete row and clear it
-					for y in range(board_h):
-						if all(board[y*board_w+x] is not None for x in range(board_w)):
-							board[board_w:(y+1)*board_w] = board[0:y*board_w]
-							board[0:board_w] = [None] * board_w
-							player_score += board_w * multiplier
+					for y in range(board.h):
+						if all(board.get(x,y) is not None for x in range(board.w)):
+							board.clear(y)
+							player_score += board.w * multiplier
 							multiplier += 1
 					break
 
